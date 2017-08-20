496c7a938879c7c3736667a38508d824362a2e1a
Keith Mason <keith.mason@conning.com>
2017-08-20 15:19:56 -0400

    flesh out Transaction type

--- src/Checkbook.jl
+++ src/Checkbook.jl
@@ -5,7 +5,13 @@ module Checkbook
 export Transaction, Account
 
 type Transaction
+    date::Date
+    description::String
+    amount::Float64
+    Transaction(description::String) = new(Date(now()), description, 0.0)
 end
+
+Transaction() = Transaction("")
 
 type Account
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -16,6 +16,24 @@ using Base.Test
         # There is an Transaction type, and it is exported.
         @test isa(transaction, Transaction)
 
+        # Transaction contains a date; by default that date is today, an
+        # empty string for a description, and a zero dollar amount.
+        @test transaction.date == Date(now())
+        @test transaction.description == ""
+        @test transaction.amount === 0.0
+
+        # Transaction's date can be updated.
+        transaction.date = Date("2017-01-01")
+        @test transaction.date == Date("2017-01-01")
+
+        # Transaction's description can be updated.
+        transaction.description = "Initial balance"
+        @test transaction.description == "Initial balance"
+
+        # Transaction's amount can be updated.
+        transaction.amount += 5
+        @test transaction.amount === 5.0
+
     end
 
     @testset "Account" begin
