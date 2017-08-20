573910d552d5fe527d027cdf75501e0604501190
Keith Mason <keith.mason@conning.com>
2017-08-20 13:40:43 -0400

    add basic object types

--- src/Checkbook.jl
+++ src/Checkbook.jl
@@ -1,4 +1,13 @@
 # Copyright 2017 Keith Mason
 
 module Checkbook
+
+export Transaction, Account
+
+type Transaction
+end
+
+type Account
+end
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,31 @@
 using Base.Test
 
-using Checkbook
+# Test infrastructure is working.
 @test true
+
+@testset "Checkbook" begin
+
+    # Checkbook module is available
+    using Checkbook
+
+    @testset "Transaction" begin
+
+        # There is a constructor for an Transaction.
+        transaction = Transaction()
+
+        # There is an Transaction type, and it is exported.
+        @test isa(transaction, Transaction)
+
+    end
+
+    @testset "Account" begin
+
+        # There is a constructor for an Account.
+        account = Account()
+
+        # There is an Account type, and it is exported.
+        @test isa(account, Account)
+
+    end
+
+end
